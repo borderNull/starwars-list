198130b50abc2b9356eb749bf1230aa2b06c73ec
borderNull <spbtvtower@gmail.com>
2023-08-10 14:29:25 +0300

    add some fixes to gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/starwars-list",
+  base: "/starwars-list/",
   plugins: [react()],
   test: {
     globals: true,
